f51259143e35c2b7a2c2b0db0b8b5276c6619576
Kartik <asooja@gmail.com>
2015-10-15 17:05:43 +0100

    minor

--- src/main/java/edu/insight/safrank/topics/TopicwiseCurveFitter.java
+++ src/main/java/edu/insight/safrank/topics/TopicwiseCurveFitter.java
@@ -1,6 +1,8 @@
-
 package edu.insight.safrank.topics;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.IOException;
 import java.util.Collections;
 import java.util.Comparator;
 import java.util.HashMap;
@@ -21,6 +23,10 @@ public class TopicwiseCurveFitter {
 	private static int startYear = 2000;
 	private static int targetYear = 2014;
 	private static int yearDiff = 2;
+	private static String whatsDoneFilePath = "src/main/resources/whatsDone.txt";
+	private static Map<String, Double> topicPredicted = new HashMap<String, Double>();
+	private static StringBuilder bl = new StringBuilder();
+
 
 	public static void linearCurveFitter() {
 		SafDataReader sReader = new SafDataReader();
@@ -45,27 +51,55 @@ public class TopicwiseCurveFitter {
 		writeFile(topicPredicted);
 	}
 
+	public static void readWhatsDone(){
+		if(new File(whatsDoneFilePath).exists()){
+			BufferedReader br = BasicFileTools.getBufferedReader(whatsDoneFilePath);
+			String line = null;
+			try {
+				while((line=br.readLine())!=null){
+					String[] split = line.split("\\t");
+					topicPredicted.put(split[0], Double.parseDouble(split[1]));
+					bl.append(split[0] + "\t" + Double.parseDouble(split[1]) + "\n");
+				}
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+		}
+	}
+
+	public static void writeWhatsDone(){
+		BasicFileTools.writeFile(whatsDoneFilePath, bl.toString().trim());
+		System.out.println("saved");
+	}
+
 	public static void gaussianCurveFitter() {
 		SafDataReader sReader = new SafDataReader();
 		Map<String, Map<Integer, SafTopicData>> topicYearSafDataMap = sReader.getTopicYearSafDataMap(targetYear);
-		Map<String, Double> topicPredicted = new HashMap<String, Double>();
-		int counter = 0;
+		readWhatsDone();
+		int counter = topicPredicted.size();
+		int writeEveryNth = 500;
 		for(String topic : topicYearSafDataMap.keySet()){
-			System.out.println(counter+++"/"+topicYearSafDataMap.size());
-			WeightedObservedPoints obs = new WeightedObservedPoints();
-			Map<Integer, SafTopicData> map = topicYearSafDataMap.get(topic);
-			for(int sY=startYear; sY<targetYear; sY = sY + yearDiff){
-				double x = sY;
-				double y = 0.0;
-				if(map.get(sY)!=null){
-					SafTopicData safData = map.get(sY);
-				//	System.out.println(topic + " " + sY + " " + safData.safVal);
-					y = safData.safVal;
+			if(!topicPredicted.containsKey(topic)){
+				System.out.println(counter++ +"/"+topicYearSafDataMap.size());
+				WeightedObservedPoints obs = new WeightedObservedPoints();
+				Map<Integer, SafTopicData> map = topicYearSafDataMap.get(topic);
+				for(int sY=startYear; sY<targetYear; sY = sY + yearDiff){
+					double x = sY;
+					double y = 0.0;
+					if(map.get(sY)!=null){
+						SafTopicData safData = map.get(sY);
+						//	System.out.println(topic + " " + sY + " " + safData.safVal);
+						y = safData.safVal;
+					}
+					obs.add(x, y);
 				}
-				obs.add(x, y);
+				Double predictSafVal = GaussianCurveFitterTest.getVal(obs, targetYear);
+				bl.append(topic + "\t" + predictSafVal + "\n");
+				topicPredicted.put(topic, predictSafVal);
+				if(counter%writeEveryNth==0){
+					writeWhatsDone();
+				}
 			}
-			Double predictSafVal = GaussianCurveFitterTest.getVal(obs, targetYear);
-			topicPredicted.put(topic, predictSafVal);
 		}
 		writeFile(topicPredicted);
 	}
@@ -84,11 +118,8 @@ public class TopicwiseCurveFitter {
 	}
 
 	private static Map<String, Double> sortByComparator(Map<String, Double> unsortMap) {
-		// Convert Map to List
 		List<Map.Entry<String, Double>> list = 
 				new LinkedList<Map.Entry<String, Double>>(unsortMap.entrySet());
-
-		// Sort list with comparator, to compare the Map values
 		Collections.sort(list, new Comparator<Map.Entry<String, Double>>() {
 			public int compare(Map.Entry<String, Double> o1,
 					Map.Entry<String, Double> o2) {
@@ -96,8 +127,6 @@ public class TopicwiseCurveFitter {
 				return (-1 * compareTo);
 			}
 		});
-
-		// Convert sorted map back to a Map
 		Map<String, Double> sortedMap = new LinkedHashMap<String, Double>();
 		for (Iterator<Map.Entry<String, Double>> it = list.iterator(); it.hasNext();) {
 			Map.Entry<String, Double> entry = it.next();
